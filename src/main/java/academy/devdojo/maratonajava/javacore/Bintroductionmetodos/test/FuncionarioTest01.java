05b968730970fae96b1818e559e65e9ed9bd0b3b
Guiberti <guiberti1227@gmail.com>
2024-03-26 17:00:28 -0300

    alteracaoloja26/03Tarde

--- src/main/java/academy/devdojo/maratonajava/javacore/Bintroductionmetodos/test/FuncionarioTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Bintroductionmetodos/test/FuncionarioTest01.java
@@ -2,14 +2,24 @@ package academy.devdojo.maratonajava.javacore.Bintroductionmetodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroductionmetodos.domain.Funcionario;
 
+import java.util.Scanner;
+
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
+
+        lerDados();
 
         funcionario.nome = "Sanji";
         funcionario.idade = 26;
         funcionario.salarios = (new double[]{1580, 987, 352});
 
+
         funcionario.imprimir();
+    }
+
+    private static void lerDados() {
+        System.out.printf("Insira o seu nome: ");
+        String nome = new Scanner(System.in).nextLine();
     }
 }
