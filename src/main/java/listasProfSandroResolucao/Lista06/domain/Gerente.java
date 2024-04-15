e2296a5930f21bde2a49c1f537da9ea4ee13dfd5
Guiberti <guiberti1227@gmail.com>
2024-04-15 20:27:50 -0300

    modificacaonoite15/04

---
+++ src/main/java/listasProfSandroResolucao/Lista06/domain/Gerente.java
@@ -0,0 +1,124 @@
+package listasProfSandroResolucao.Lista06.domain;
+
+public class Gerente extends Pessoa {
+
+
+    Loja loja;
+    double salarioBase;
+    double[] salarioRecebido = new double[3];
+
+    private Gerente(String nomePessoa, Integer idade, String estado, String cidade, String bairro, String rua,
+                    Integer numero, String complemento, Loja loja, double salarioBase, double[] salarioRecebido) {
+        super(nomePessoa, idade, estado, cidade, bairro, rua, numero, complemento);
+        this.loja = loja;
+        this.salarioBase = salarioBase;
+        this.salarioRecebido = salarioRecebido;
+    }
+
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    @Override
+    public String apresentarse() {
+        return ("Meu nome é: "
+                .concat(nomePessoa)
+                .concat(", tenho idade é: ")
+                .concat(String.valueOf(idade))
+                .concat(", sou gerente da loja: ")
+                .concat((loja == null) ? "null" : loja.getNomeFantasia())
+                .concat("."));
+    }
+
+    public double calcularMedia() {
+        double soma = 0;
+        for (double salario : salarioRecebido) {
+            soma += salario;
+        }
+        return soma / salarioRecebido.length;
+    }
+
+    public double calcularBonus() {
+        return salarioBase * 0.35;
+    }
+
+    public static final class GerenteBuilder {
+        private String estado;
+        private String cidade;
+        private String bairro;
+        private String rua;
+        private Integer numero;
+        private String complemento;
+        private Loja loja;
+        private double salarioBase;
+        private double[] salarioRecebido;
+        private String nomePessoa;
+        private Integer idade;
+
+        private GerenteBuilder() {
+        }
+
+        public static GerenteBuilder builder() {
+            return new GerenteBuilder();
+        }
+
+        public GerenteBuilder estado(String estado) {
+            this.estado = estado;
+            return this;
+        }
+
+        public GerenteBuilder cidade(String cidade) {
+            this.cidade = cidade;
+            return this;
+        }
+
+        public GerenteBuilder bairro(String bairro) {
+            this.bairro = bairro;
+            return this;
+        }
+
+        public GerenteBuilder rua(String rua) {
+            this.rua = rua;
+            return this;
+        }
+
+        public GerenteBuilder numero(Integer numero) {
+            this.numero = numero;
+            return this;
+        }
+
+        public GerenteBuilder complemento(String complemento) {
+            this.complemento = complemento;
+            return this;
+        }
+
+        public GerenteBuilder loja(Loja loja) {
+            this.loja = loja;
+            return this;
+        }
+
+        public GerenteBuilder salarioBase(double salarioBase) {
+            this.salarioBase = salarioBase;
+            return this;
+        }
+
+        public GerenteBuilder salarioRecebido(double[] salarioRecebido) {
+            this.salarioRecebido = salarioRecebido;
+            return this;
+        }
+
+        public GerenteBuilder nomePessoa(String nomePessoa) {
+            this.nomePessoa = nomePessoa;
+            return this;
+        }
+
+        public GerenteBuilder idade(Integer idade) {
+            this.idade = idade;
+            return this;
+        }
+
+        public Gerente build() {
+            return new Gerente(nomePessoa, idade, estado, cidade, bairro, rua, numero, complemento, loja, salarioBase, salarioRecebido);
+        }
+    }
+}
---
+++ src/main/java/listasProfSandroResolucao/Lista06/domain/Loja.java
@@ -0,0 +1,149 @@
+package listasProfSandroResolucao.Lista06.domain;
+
+public class Loja extends Endereco {
+    private String nomeFantasia;
+    private String razaoSocial;
+    private Long cnpj;
+    private Vendedor[] vendedores;
+    private Cliente[] clientes;
+
+    private Loja(String estado, String cidade, String bairro, String rua, Integer numero, String complemento,
+                 String nomeFantasia, String razaoSocial, Long cnpj, Vendedor[] vendedores, Cliente[] clientes) {
+        super(estado, cidade, bairro, rua, numero, complemento);
+        this.nomeFantasia = nomeFantasia;
+        this.razaoSocial = razaoSocial;
+        this.cnpj = cnpj;
+        this.vendedores = vendedores;
+        this.clientes = clientes;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public Long getCnpj() {
+        return cnpj;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    // met add
+    public int contarClientes() {
+        return clientes.length;
+    }
+
+    public int contarVendedores() {
+        return vendedores.length;
+    }
+
+    public String apresentarse() {
+        return ("Nome Fantasia: "
+                .concat(nomeFantasia)
+                .concat(", CNPJ: ")
+                .concat(String.valueOf(cnpj))
+                .concat(", Endereço: ")
+                .concat(" Cidade: ")
+                .concat(cidade)
+                .concat(", Bairro: ")
+                .concat(bairro)
+                .concat(", Rua: ")
+                .concat(rua)
+                .concat(".")
+        );
+    }
+
+
+    public static final class LojaBuilder {
+        private String estado;
+        private String cidade;
+        private String bairro;
+        private String rua;
+        private Integer numero;
+        private String complemento;
+        private String nomeFantasia;
+        private String razaoSocial;
+        private Long cnpj;
+        private Vendedor[] vendedores;
+        private Cliente[] clientes;
+
+        private LojaBuilder() {
+        }
+
+        public static LojaBuilder builder() {
+            return new LojaBuilder();
+        }
+
+        public LojaBuilder estado(String estado) {
+            this.estado = estado;
+            return this;
+        }
+
+        public LojaBuilder cidade(String cidade) {
+            this.cidade = cidade;
+            return this;
+        }
+
+        public LojaBuilder bairro(String bairro) {
+            this.bairro = bairro;
+            return this;
+        }
+
+        public LojaBuilder rua(String rua) {
+            this.rua = rua;
+            return this;
+        }
+
+        public LojaBuilder numero(Integer numero) {
+            this.numero = numero;
+            return this;
+        }
+
+        public LojaBuilder complemento(String complemento) {
+            this.complemento = complemento;
+            return this;
+        }
+
+        public LojaBuilder nomeFantasia(String nomeFantasia) {
+            this.nomeFantasia = nomeFantasia;
+            return this;
+        }
+
+        public LojaBuilder razaoSocial(String razaoSocial) {
+            this.razaoSocial = razaoSocial;
+            return this;
+        }
+
+        public LojaBuilder cnpj(Long cnpj) {
+            this.cnpj = cnpj;
+            return this;
+        }
+
+        public LojaBuilder vendedores(Vendedor[] vendedores) {
+            this.vendedores = vendedores;
+            return this;
+        }
+
+        public LojaBuilder clientes(Cliente[] clientes) {
+            this.clientes = clientes;
+            return this;
+        }
+
+        public Loja build() {
+            return new Loja(estado, cidade, bairro, rua, numero, complemento, nomeFantasia, razaoSocial, cnpj, vendedores, clientes);
+        }
+    }
+}
