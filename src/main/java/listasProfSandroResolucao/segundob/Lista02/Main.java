ebc0b0ce802fd66443e8bfaa97bde577cdc3ecf8
Guiberti <guiberti1227@gmail.com>
2024-05-18 00:43:38 -0300

    moite

--- src/main/java/listasProfSandroResolucao/segundob/Lista02/Main.java
+++ src/main/java/listasProfSandroResolucao/segundob/Lista02/Main.java
@@ -8,26 +8,32 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ATV1_________");
         atvUm();
-        System.out.println("ATV2_________");
+        System.out.println("\nATV2_________");
         atvDois();
-        System.out.println("ATV3_________");
+        System.out.println("\nATV3_________");
         atvTres();
-        System.out.println("ATV4_________");
+        System.out.println("\nATV4_________");
         atvQuatro();
-        System.out.println("ATV5_________");
+        System.out.println("\nATV5_________");
     }
 
     public static void atvUm() {
-        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8));
-        List<Integer> pairNumbers = numbers.stream().map(number -> {
-            Integer numeroPar = number % 2 == 0;
-
-        })
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16));
+        List<Integer> pairNumbers = numbers.stream().filter(n -> n % 2 == 0)
                 .toList();
+
+        pairNumbers.forEach(number -> System.out.println(number));
     }
 
     public static void atvDois() {
+        List<String> names = new ArrayList<>();
+        names.add("roberto");
+        names.add("josé");
+        names.add("caio");
+        names.add("vinicius");
 
+        List<String> capitalizedNames = names.stream().map(String::toUpperCase).toList();
+        capitalizedNames.forEach(name -> System.out.println(name));
     }
 
     public static void atvTres() {
