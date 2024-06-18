187b5190e68bd521ddd6bcc72197cfeda3a23584
Guiberti <guiberti1227@gmail.com>
2024-06-18 17:02:00 -0300

    alteracaomanhaloja14/05

--- src/main/java/listasProfSandroResolucao/primeirob/Avaliacoes/Prova02/test/Main.java
+++ src/main/java/listasProfSandroResolucao/primeirob/Avaliacoes/Prova02/test/Main.java
@@ -1,8 +1,21 @@
 package listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.test;
 
+import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.EventoRepository;
+import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.IngressoRepository;
+
 public class Main {
     public static void main(String[] args) {
+        IngressoRepository ingressoRepository = new IngressoRepository();
+        EventoRepository eventoRepository = new EventoRepository();
 
 
+        while (true) {
+            impressaoConsole();
+        }
+
+    }
+
+    private static void impressaoConsole() {
+        System.out.println("Insira uma das opções abaixo: ");
     }
 }
--- src/main/java/listasProfSandroResolucao/primeirob/Avaliacoes/Prova02/test/Test.java
+++ src/main/java/listasProfSandroResolucao/primeirob/Avaliacoes/Prova02/test/Test.java
@@ -1,16 +1,18 @@
 package listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.test;
 
-import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.Cliente;
-import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.Endereco;
-import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.Evento;
-import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.Ingresso;
+import listasProfSandroResolucao.primeirob.Avaliacoes.Prova02.domain.*;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class Test {
     public static void main(String[] args) {
+
+        IngressoRepository ingressoRepository = new IngressoRepository();
+
         Ingresso ingressoTest = new Ingresso(1, null, null, "ingresso dia 1", 2
                 , LocalDate.of(2024, 06, 21), 75d);
+        ingressoRepository.save(ingressoTest);
 
         Evento eventoTest = new Evento(1, "Buteco do Embaixador", 1500, 150d,
                 LocalDate.of(2024, 06, 20), LocalDate.of(2024, 06, 23));
@@ -33,6 +35,13 @@ public class Test {
         System.out.println("------");
         System.out.println(enderecoTest.toString());
         System.out.println("------");
+
+        Optional<Ingresso> foundIngresso = ingressoRepository.findById(1);
+        if (foundIngresso.isPresent()) {
+            System.out.println("Ingresso encontrado: " + foundIngresso.get());
+        } else {
+            System.out.println("Ingresso com ID " + 1 + " não encontrado.");
+        }
 
 
     }
